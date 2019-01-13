282d5f67f15b246068277158c506f62ab4ffee63
Tina Zhao <tinazhao@berkeley.edu>
2019-01-12 23:33:41 -0800

    Updated README and accounted for some edge cases

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# jokebot
-Take-home assignment for CSM Tech Committee Application
+# Jokebot
+My submisson for the take-home assignment for the Spring 2019 CSM Tech Committee Application.
+
+## Functionality
+The purpose of the Jokebot is to deliver prompts and punchlines to the user. Then, the user can type `next` to hear another joke or `quit` to stop the program. If the Jokebot runs out of jokes, the program stops. 
+
+You can run program from the command line with Option 1: `python jokebot.py` or Option 2: `python jokebot.py file_name.csv`. Option 1 reads jokes from /r/dadjokes. Option 2 reads jokes for a provided csv file. The file must be in the same directory as `jokebot.py`.
+
+## Edges Cases
+### Involving System Arguments
+* **Incorrect formatting of CSV file name:** prints "Please reference a CSV file" and ends program
+* **More than 2 arguments passed in:** prints "Unable to interpret command" and ends program
+
+### Involving CSV file
+* **Does not exist/Not in current directory:** prints "Not in current directory" and ends program
+* **Corrupted file/Incorrect format(i.e. empty row, missing columm)/No complete jokes:** prints "Unable to read CSV file" and ends program
+* Empty prompt or punchline: Skips joke
+
+### Involving Reddit Jokes 
+* **No Internet Connection/Changes made to Reddit JSON:** prints "Unable to process jokes from Reddit" and ends program
+* **No jokes match guidelines:** prints "No jokes available. Check back later." and ends program
+
+
+
--- jokebot.py
+++ jokebot.py
@@ -1,28 +1,41 @@
-import time, csv, sys, requests, os
+import csv
+import os
+import requests
+import sys
+import time
+
 
 #main function
-def run_jokebot():
+def main():
 	jokes_list = get_jokes()
-	num_deliv_jokes = 0
+	num_deliv_jokes = 0 #tracks the number of jokes delievered
 	for joke in jokes_list:
 		deliver_joke(joke[0], joke[1])
 		num_deliv_jokes += 1
-		if num_deliv_jokes != len(jokes_list) and not read_input():
+		if num_deliv_jokes != len(jokes_list) and not read_user_input(): #avoids prompting the user after delivering the last joke
 			break
+
+def deliver_error_message(msg):
+	print(msg)
+	sys.exit(0)
 
 #returns a list of jokes depending on command line args
 def get_jokes(): 
 	if len(sys.argv) == 1:
 		jokes_list = reddit_jokes()
-	else:
+		if not jokes_list:
+			deliver_error_message("No jokes available. Check back later.")
+	elif len(sys.argv) == 2:
 		csv_file = sys.argv[1]
+		if csv_file.split(".")[-1] != "csv":
+			deliver_error_message("Please reference a CSV file.")
 		if not os.path.isfile(csv_file):
-			print("Cannot find joke file in current directory")
-			sys.exit(0)
+			deliver_error_message("Cannot find joke file in current directory.")
 		jokes_list = read_csv(csv_file)
 		if not jokes_list:
-			print("Unable to read CSV file")
-			sys.exit(0)
+			deliver_error_message("Unable to read CSV file.")
+	else:
+		deliver_error_message("Unable to interpret command.")
 	return jokes_list
 
 #helper function that reads CSV file and returns list of tuples
@@ -31,22 +44,37 @@ def read_csv(file_name):
 	try:
 		with open(file_name) as csv_file:
 			table = csv.reader(csv_file, delimiter=',')
-			return [(row[0], row[1]) for row in table]
+			return [(row[0], row[1]) for row in table if row[0] and row[1]]
 	except:
 		return False
 
-#times the delivery of prompt and punchline 
-#if prompt/punchline are missing, skip line
 def deliver_joke(prompt, punchline):
-	if prompt:
+	''' Delivers the prompt and punchline of a joke.
+	
+	Args: 
+		prompt: string for joke setup
+		punchline: string for joke punchline
+	Returns:
+		
+	'''
+	if prompt and punchline:
 		print(prompt)
-		if punchline:
-			time.sleep(2)
-	if punchline:
+		time.sleep(2)
 		print(punchline)
 
-#reads user input
-def read_input():
+def read_user_input():
+	'''Prompts for and responds to user input. 
+
+	Encourages user to input readable commands. 
+
+	Args: None
+
+	Returns: 
+		A boolean value. 
+		Returns True if user responds with 'next'. False if response is 'quit'.
+		Prints 'I don't understand' and prompts user again. 
+
+	'''
 	while True:
 		time.sleep(1)
 		response = input("Type next for another joke and quit to stop. \n")
@@ -57,16 +85,38 @@ def read_input():
 		else:
 			print("I don't understand.")
 
-#returns list of dad jokes from Reddit
-#filters out over 18 jokes and ensures joke is a question
 def reddit_jokes():
-	json_dict = requests.get("https://www.reddit.com/r/dadjokes.json", headers = {'User-agent': 'jokebot'}).json()
-	title_list = ['how','why','what']
-	return [(joke['data']['title'], joke['data']['selftext']) for joke in json_dict['data']['children'] if not joke['data']['over_18'] and joke['data']['title'].split(' ')[0].lower() in title_list and not joke['data']['title'].split('?')[-1]]
+	''' Filters out and returns a list of dad jokes for /r/dadjokes. 
+
+	Filter guidelines: 
+		1. Joke is not intended for an over 18 audience. 
+		2. Prompt is a question.
+			a. First word is a common interrogative word
+			b. Ends with a question mark
+
+	Args: None
+	Returns:
+		A list of tuples containing the prompt and the punchline of the joke. 
+		example:
+
+			[("What is brown and sticky?", "A stick."), 
+			("What do you call an elephant that doesn't matter?", "An irrelephant."),
+			("What do you call a man with a rubber toe?", "Roberto.")]
+	'''
+	try:
+		json_dict = requests.get("https://www.reddit.com/r/dadjokes.json", headers = {'User-agent': 'jokebot'}).json()
+		interr_word_list = ['how','why','what', 'is', 'who', 'if', 'when', 'which']
+		return [(joke['data']['title'], joke['data']['selftext']) 
+			for joke in json_dict['data']['children'] #iterates through each Reddit post 
+				if not joke['data']['over_18'] #checks if joke is not over 18
+				and joke['data']['title'].split(' ')[0].lower() in interr_word_list  #checks for interrogative word
+				and not joke['data']['title'].split('?')[-1]] #checks for question mark
+	except: 
+		deliver_error_message("Unable to process jokes from Reddit")
 
 
 if __name__ == "__main__":
-	run_jokebot()
+	main()
 
 
 
